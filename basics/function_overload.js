2ee64f25a75424e09df00afc9bf54486dfe258ff
Tomasz <dworowytomasz@gmail.com>
2019-04-26 16:54:40 +0200

    update gitignore, remove js files

--- basics/function_overload.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function add(a, b) {
-    return a + b;
-}
-console.log(`add(1,1)= ${add(1, 1)}`);
-console.log(`add('1','1')= ${add('1', '1')}`);
--- oop/interfaces.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let complexType;
-complexType = { id: 1, name: 'Test' };
-console.log(complexType);
-let complexType2;
-complexType2 = { id: 1 };
-console.log(complexType2);
