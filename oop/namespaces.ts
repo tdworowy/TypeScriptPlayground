373e096eb1127f47b8a0ba1f0c64d02f1ad74d00
Tomasz <dworowytomasz@gmail.com>
2019-04-28 14:45:20 +0200

    update

--- oop/namespaces.ts
+++ oop/namespaces.ts
@@ -1,10 +1,10 @@
 namespace TestNameSpace {
-class NotExported {} // invisible outside namespace
-export class Exported { // visible outsice nmasepace
-    printsmt(){
-        console.log("SMT")
-    }
-} 
+    class NotExported {} // invisible outside namespace
+    export class Exported { // visible outsice nmasepace
+        printsmt(){
+            console.log("SMT")
+        }
+    } 
 }
 let testObj =  new TestNameSpace.Exported()
 testObj.printsmt()
