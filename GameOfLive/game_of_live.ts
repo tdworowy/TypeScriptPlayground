e3364b533b4a8ec524c79803dedcf46da02a66cf
Tomasz <dworowytomasz@gmail.com>
2019-05-13 14:31:26 +0200

    add working game of live

--- GameOfLive/game_of_live.ts
+++ GameOfLive/game_of_live.ts
@@ -2,19 +2,27 @@
 interface IFrontend {
     isAlive(cellId:number): boolean
     kill(cellId:number): void
-    revive(cellId:number): void 
+    revive(cellId:number): void
 }
+  
 interface IGameOfLive {
-    startGame(): void
+    startGame(initialCellsCount:number): void
+    checkRules():void
+    play():void
+}
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
 }
 class GameOfLive implements IGameOfLive{
 
     private frontEnd: IFrontend
     private rowsCount: number
+    private itemsCount: number
     
     constructor(frontEnd: IFrontend, rowsCount:number) {
         this.frontEnd = frontEnd
         this.rowsCount = rowsCount
+        this.itemsCount = this.rowsCount * this.rowsCount 
     }
     private getNigbursIndexes (cellID: number): number[] {
         return [ cellID - 1,
@@ -28,19 +36,107 @@ class GameOfLive implements IGameOfLive{
                  cellID +  this.rowsCount + 1 ] 
     }   
     private isAlive(cellId:number): boolean {
-        return this.frontEnd.isAlive(cellId)
+        try {
+            return this.frontEnd.isAlive(cellId)
+        }
+        catch(TypeError) {
+            console.log(`NULL at: ${cellId}`)
+        }
+        return false
     }
+    private revive(cellId:number) {
+        this.frontEnd.revive(cellId)
+    }
+    private kill(cellId:number) {
+        this.frontEnd.kill(cellId)
+    }
+    
     private getLiveNigbursCount(cellID: number) : number {
         var LiveNigbursCount = 0;
     
         for(var index of this.getNigbursIndexes(cellID)) {
-            if(this.isAlive(index)) {
-                LiveNigbursCount = +1
+            if(index >=0 && index < this.itemsCount) {  
+                    if(this.isAlive(index)) {
+                        LiveNigbursCount += 1
+                    }
             }
         }
         return LiveNigbursCount
     }
-    startGame(): void {
-
+    checkRules() {
+        for(var i:number=0;i< this.itemsCount;i++) {
+            console.log(`Check ID: ${i}`)
+            var LiveNigbursCount = this.getLiveNigbursCount(i)
+            console.log(`LiveNigbursCount: ${LiveNigbursCount}`)
+            if(this.isAlive(i)) {
+                switch(true) {
+                    case LiveNigbursCount < 2: {
+                        this.kill(i)
+                        console.log(`Kill: ${i}`)
+                        break
+                    }
+                    case LiveNigbursCount > 3: {
+                        this.kill(i)
+                        console.log(`Kill: ${i}`)
+                        break
+                    }
+                } 
+            }
+            if(!this.isAlive(i)){
+                if(LiveNigbursCount == 3) {
+                   this.revive(i)
+                   console.log(`revive: ${i}`)    
+                }
+            }
+        }
+    }
+    startGame(initialCellsCount:number): void {
+        for(let i=0;i< initialCellsCount;i++) {
+          var index: number = Math.floor(Math.random() * (this.itemsCount));
+          this.revive(index)
+        }
+    }
+    async play() {
+        while(1) {
+            this.checkRules()
+            await delay(500)
+        }
     }
 }
+class WebFrontEnd implements IFrontend {
+    private elements: HTMLCollectionOf<Element>
+    constructor() {
+        (async () => { 
+            await delay(1000);
+        })();
+        this.elements = document.getElementById('grid-container').getElementsByClassName('grid-item')
+    }
+    isAlive(cellId:number) {
+      if(this.elements.item(cellId).getAttribute('alive') == 'true') {
+          return true
+      }
+      else {
+          return false
+      }
+    }
+    revive(cellId:number) {
+        this.elements.item(cellId).setAttribute('alive','true') 
+        this.elements.item(cellId).setAttribute('style','background-color: red;') 
+    }
+    kill(cellId:number) {
+        this.elements.item(cellId).setAttribute('alive','false') 
+        this.elements.item(cellId).setAttribute('style','background-color: blue;') 
+    }
+    
+}
+var first = true
+var frontEnd: IFrontend
+var game: IGameOfLive
+window.onload = () => {
+    if(first) {
+        frontEnd = new WebFrontEnd()
+        game = new GameOfLive(frontEnd, 50);
+        game.startGame(200)
+        first = false
+    }
+};
--- GameOfLive/generate_html.py
+++ GameOfLive/generate_html.py
@@ -1,16 +1,23 @@
 
 
 def generate_grid_items(count):
-   return "".join([f'<div id = {i} class="grid-item">{i}</div>\n' for i in range(count)])
+   return "".join([f'<div id = {i} alive=false class="grid-item">{i}</div>\n' for i in range(count)])
 
-def generate_gird(template, rows_count, columns_count):
+def generate_gird(template, rows_count):
+    """
+    Generate html file.
+    :param template: html template
+    :param rows_count: number of grid rows (columns will be the same)
+    :return: new html
+    :rtype: str
+    """
     auto = 'auto ' * rows_count
     with open(template) as file:
         new_html = file.read()
     new_html = new_html.replace('{% COLUMNS %};', f'grid-template-columns: {auto};\n')
-    new_html = new_html.replace('{% ITEM %};', generate_grid_items(rows_count * columns_count))
+    new_html = new_html.replace('{% ITEM %};', generate_grid_items(rows_count * rows_count))
     return new_html
 
 if __name__ == "__main__":
     with open('index.html', 'w') as index:
-        index.write(generate_gird("template.html", 50, 50))
+        index.write(generate_gird("template.html", 50))
