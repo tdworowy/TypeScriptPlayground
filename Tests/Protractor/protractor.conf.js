acfc4f9a5c4198301aea111c1ed4d682a1625297
Tomasz <dworowytomasz@gmail.com>
2019-05-22 14:37:11 +0200

    update

--- Tests/Protractor/protractor.conf.js
+++ Tests/Protractor/protractor.conf.js
@@ -1,4 +1,11 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['*.js']
+    specs: ['*.js'],
+    onPrepare: function() {
+        var jasmineReporters = require('jasmine-reporters')
+        jasmine.getEnv().addReporter(
+            new jasmineReporters.TeamCityReporter()
+        )
+    }
+
 }
