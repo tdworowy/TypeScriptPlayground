b1fc93cacb511ee6359ba306af92f89fddeb4e06
Tomasz <dworowytomasz@gmail.com>
2019-06-11 10:51:23 +0200

    update

--- full_stack_example/full_stack_app/src/components/App.js
+++ full_stack_example/full_stack_app/src/components/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
 //import axios from 'axios'
 import Header from "./Header"
-import ContestPreview from './ContestPreview'
+import ContestList from "./ContestsList"
+import Contest from "./Contest"
 
+
+const pushState = (obj, url) => window.history.pushState(obj,'',url)
 
 class App extends React.Component {
-    // constructor(props) {
-    //     super(props)
-    // }
-
     state = {
         pageHeader: "Naming Contests",
         contests: this.props.initialContests
@@ -29,16 +28,33 @@ class App extends React.Component {
     componentWillUnmount() {
         console.log("Will Unmount")
     }
+    fetchContest = (contestId) => {
+        pushState(
+            {currentContestId: contestId},
+            `/contest/${contestId}`
+        )
+        this.setState({
+            pageHeader: this.state.contests[contestId].contestName,
+            currentContestId: contestId
+        })
+    }
+    currentContent() {   
+        if(this.state.currentContestId) {
+            return <Contest {...this.state.contests[this.state.currentContestId]} />
+        }
+        else
+            return <ContestList 
+                    onContestClick = {this.fetchContest}
+                    contests = {this.state.contests} />
+
+    }
     render() {
         console.log(this.state.contests)
         return (
             <div className="App">
                <Header message={this.state.pageHeader}/>
-                <div>
-                 {this.state.contests.map(contest =>
-                    <ContestPreview key={contest.id} {...contest}/>
-                    )}
-                </div>
+               {this.currentContent()}
+              
             </div>
         )
     }
--- full_stack_example/full_stack_app/src/components/ContestPreview.js
+++ full_stack_example/full_stack_app/src/components/ContestPreview.js
@@ -1,14 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const ContestPreview = (contest) => {
-  return( <div className="ContestPreview">
-            <div className="category-name">
-              {contest.categoryName}
-            </div>
-            <div className="contest-name">
-               {contest.contestName}
-            </div>
-          </div>)
+class ContestPreview extends React.Component {
+  
+  handleClick = () => {
+    this.props.onClick(this.props.id)
+    console.log(this.props.contestName)
+  }
+  render() {
+      return( <div className="link ContestPreview" onClick={this.handleClick}>
+      <div className="category-name">
+        {this.props.categoryName}
+      </div>
+      <div className="contest-name">
+        {this.props.contestName}
+      </div>
+    </div>)
+  }
 }
-
+ContestPreview.propTypes = {
+  id: PropTypes.number.isRequired, 
+  categoryName: PropTypes.string.isRequired,
+  contestName:  PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+}
 export default ContestPreview
