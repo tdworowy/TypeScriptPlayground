6c8802a8551028035e8d16e26531b153c50da928
Tomasz <dworowytomasz@gmail.com>
2019-06-14 09:28:06 +0200

    update

--- full_stack_example/full_stack_app/api/index.js
+++ full_stack_example/full_stack_app/api/index.js
@@ -48,5 +48,28 @@ router.get('/contests/:contestId', (req, res) => {
         .catch(console.error)
     
 })
+router.post('/names', (req, res) => {
+    const contestId = ObjectId(req.body.contestId)
+    const name = req.body.name
+    mdb.collection('names').insertOne({ name })
+        .then(result => {
+            mdb.collection('contests').findAndModify(
+                { _id: contestId},
+                [],
+                { $pusch: {nameIds: result.insertedId }},
+                { new: true }
+            ).then(doc =>{
+                res.send({
+                    updatedContest: doc.value,
+                    newName: { _id: result.insertedId, name }
+                })
+            })
+        })
+        .catch(error => {
+            console.error(error)
+            res.status(404).send("Bad request")
+        })
+})
+
 
 export default router
--- full_stack_example/full_stack_app/server.js
+++ full_stack_example/full_stack_app/server.js
@@ -3,9 +3,10 @@ import config from './config'
 import apiRouter from './api'
 import sassMiddleware from 'node-sass-middleware'
 import path from 'path'
-
+import bodyParser from "body-parser"
 const server = express()
 
+server.use(bodyParser.json())
 server.use(sassMiddleware({
     src: path.join(__dirname, 'sass'),
     dest: path.join(__dirname, 'public')
