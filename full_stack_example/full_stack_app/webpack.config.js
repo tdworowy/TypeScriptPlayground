fa07fd2c9e5a970c164b882d1662bedae59355e2
Tomasz <dworowytomasz@gmail.com>
2019-06-15 11:48:19 +0200

    update

--- full_stack_example/full_stack_app/webpack.config.js
+++ full_stack_example/full_stack_app/webpack.config.js
@@ -6,10 +6,6 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.json$/,
-      //   loader: 'json-loader'
-      // },
       {
         test: /\.js$/,
         loader: 'babel-loader'
