bbf5eb9226dfa716928623a92adf1d3b33a12f05
Tomasz <dworowytomasz@gmail.com>
2019-06-16 16:24:28 +0200

    update

---
+++ full_stack_example/full_stack_app/serverRender.jsx
@@ -0,0 +1,33 @@
+let axios = require("axios");
+import ReactDOMServer from 'react-dom/server';
+import App from './src/components/App';
+import config from './config';
+import React from 'react';
+const getApiUrl = (contestId) => {
+    if (contestId) {
+        return `${config.serverUrl}/api/contests/${contestId}`;
+    }
+    return `${config.serverUrl}/api/contests`;
+};
+const getInitialData = (contestId, apiData) => {
+    if (contestId) {
+        return {
+            currentContestId: apiData._id,
+            contests: {
+                [apiData._id]: apiData
+            }
+        };
+    }
+    return {
+        contests: apiData.contests
+    };
+};
+const serverRender = (contestId) => axios.get(getApiUrl(contestId))
+    .then((resp) => {
+    const initialData = getInitialData(contestId, resp.data);
+    return {
+        initialMarkup: ReactDOMServer.renderToString(<App initialData={initialData}/>),
+        initialData: initialData
+    };
+});
+export default serverRender;
