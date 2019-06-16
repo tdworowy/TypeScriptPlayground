a80335f3024affa231b5f75978dc3a0bb9d35aa7
Tomasz <dworowytomasz@gmail.com>
2019-06-16 15:43:46 +0200

    update

--- full_stack_example/full_stack_app/server.js
+++ full_stack_example/full_stack_app/server.js
@@ -1,38 +1,32 @@
-import express from 'express'
-import config from './config'
-import apiRouter from './api'
-import sassMiddleware from 'node-sass-middleware'
-import path from 'path'
-import bodyParser from "body-parser"
-const server = express()
-
-server.use(bodyParser.json())
+import express from 'express';
+import config from './config';
+import apiRouter from './api/index';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
+import bodyParser from "body-parser";
+const server = express();
+server.use(bodyParser.json());
 server.use(sassMiddleware({
     src: path.join(__dirname, 'sass'),
     dest: path.join(__dirname, 'public')
-}))
-server.use(express.static('public'))
-server.use('/api', apiRouter)
-server.set('view engine', 'ejs')
-
-import serverRender from './serverRender'
-
+}));
+server.use(express.static('public'));
+server.use('/api', apiRouter);
+server.set('view engine', 'ejs');
+import serverRender from './serverRender';
 server.get(['/', '/contest/:contestId'], (req, res) => {
     serverRender(req.params.contestId)
-     .then(( {initialMarkup, initialData} ) => {
+        .then(({ initialMarkup, initialData }) => {
         res.render('index', {
             initialMarkup,
             initialData
-          });
-     })
-     .catch(error => {
-         console.error(error)
-         res.status(404).send("Bad request")
-     })
-    
+        });
+    })
+        .catch((error) => {
+        console.error(error);
+        res.status(404).send("Bad request");
+    });
 });
-
-
-server.listen(config.port, config.host, ()=> {
-    console.info("Express listening on port ", config.port)
-})
+server.listen(Number(config.port), config.host, () => {
+    console.info("Express listening on port ", config.port);
+});
