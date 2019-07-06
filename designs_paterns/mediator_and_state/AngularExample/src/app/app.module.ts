a9b8f6b9ca608b2d4cf3e67f7564a20586929442
Tomasz <dworowytomasz@gmail.com>
2019-07-06 10:54:13 +0200

    update

--- designs_paterns/mediator_and_state/AngularExample/src/app/app.module.ts
+++ designs_paterns/mediator_and_state/AngularExample/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar.component'
+import { SideNavComponent } from './sidenav.component'
+import { RightScreenComponent } from './rightscreen.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SideNavComponent,
+    RightScreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
