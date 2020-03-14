99bd8ee657f936037870beefcde1ec2d63cb8c7e
Tomasz <dworowytomasz@gmail.com>
2020-03-14 22:17:11 +0100

    update

--- basics/advance_types.ts
+++ basics/advance_types.ts
@@ -30,17 +30,17 @@ type numberOrString = number | string
 
 function addWithTypeGuard2 ( arg1: numberOrString,
     arg2: numberOrString ): numberOrString {
-if(typeof arg1 === 'string') {
-console.log("First argument is string")
-return arg1 + arg2
-}
-if(typeof arg1 === 'number' && typeof arg2 === 'number') {
-console.log("Both arguments are numbers")
-return arg1 + arg2
-}                          
-console.log("Default way")
-return arg1.toString() + arg2.toString()
-}
+    if(typeof arg1 === 'string') {
+        console.log("First argument is string")
+        return arg1 + arg2
+    }
+    if(typeof arg1 === 'number' && typeof arg2 === 'number') {
+        console.log("Both arguments are numbers")
+        return arg1 + arg2
+    }                          
+        console.log("Default way")
+        return arg1.toString() + arg2.toString()
+    }
 console.log(addWithTypeGuard2(1,2))
 console.log(addWithTypeGuard2('a','a'))
 console.log(addWithTypeGuard2('a',2))
